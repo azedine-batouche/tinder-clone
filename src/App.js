e7d3ae6b761522c99e0ebb13983d099030f9c0d4
azedine batouche <azedine.batouche@outlook.com>
2021-02-21 10:38:10 +0100

    core: add components
    -1 header componant
    -2 tinder cards

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
+
 import './App.css';
+import React   from 'react'
+import  Header  from "./Header";
+import TinderCard from "./TinderCard";
+import {BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <h1>Let's build the TINDER Clone  </h1>  
-        </div>
+        <Header/>
+      <Router> 
+        <Switch>
+        <Route path="/chat">
+            <h2>I am the chat page</h2>
+          </Route>
+          <Route path="/">
+            <TinderCard/>
+          </Route>
+        </Switch>
+      
+      </Router>
+  
+    </div>
   );
 }
 
