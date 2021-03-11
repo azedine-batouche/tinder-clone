22d451df977ad3741f40d646e6fbffd619f187e9
azedine batouche <azedine.batouche@outlook.com>
2021-03-11 20:57:17 +0100

    update: cleanning files

--- src/components/like/Like.js
+++ src/components/like/Like.js
@@ -4,7 +4,6 @@ import "./Like.css";
 
 function Like({ online, status, url }) {
 
-    console.log(status);
     return (
         <div className="like__ctn" >
             <img src={url} className="like__img" alt="Photo_like" />
